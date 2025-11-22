562647e3d04778b8b05feffbb9b7a77c984dd281
Sebastián Maluk <smaluk@buk.cl@users.noreply.github.com>
2025-11-22 02:47:46 -0300

    chore: prettier generated files

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -8,17 +8,17 @@
  * @module
  */
 
-import type * as myFunctions from "../myFunctions.js"
+import type * as myFunctions from "../myFunctions.js";
 
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
-} from "convex/server"
+} from "convex/server";
 
 declare const fullApi: ApiFromModules<{
-  myFunctions: typeof myFunctions
-}>
+  myFunctions: typeof myFunctions;
+}>;
 
 /**
  * A utility for referencing Convex functions in your app's public API.
@@ -31,7 +31,7 @@ declare const fullApi: ApiFromModules<{
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
->
+>;
 
 /**
  * A utility for referencing Convex functions in your app's internal API.
@@ -44,6 +44,6 @@ export declare const api: FilterApi<
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
->
+>;
 
-export declare const components: {}
+export declare const components: {};
